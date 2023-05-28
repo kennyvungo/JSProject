478a6acf03aa437c8b4e3c59b222ecb846555679
kennyvngo <kennyvungo@gmail.com>
2023-05-27 21:55:49 -0700

    finished the views for the ipod

--- src/components/ipod.js
+++ src/components/ipod.js
@@ -7,27 +7,43 @@ class Ipod{
         this.play.addEventListener("click",this.handleClick);
         this.next.addEventListener("click",this.handleClick);
         this.audio = new Audio('src/assets/stronger.mp3')
-        this.playing = false;
+        this.menu = document.querySelector(".menu")
+        this.menu.addEventListener("click",this.handleClick);
+        this.isPlaying = false;
     }
 
     handleClick(e){
-        if(e.target === this.play){
-            this.playAudio(this.playing);
-        }
-        else{
-            this.ipo.classList.add("fadeout");
-            this.ipo.classList.add("hidden");
-            let spot =document.querySelector(".spotplay");
-            spot.classList.add("fastfade");
-            spot.classList.remove("hidden")
+        switch(e.target){
+            case this.play:
+                this.isPlaying ?  this.stopAudio() : this.playAudio() ;
+                break;
+            case this.menu:
+                document.querySelector(".apple").classList.remove("hidden")
+                setTimeout(this.loadMenu,2000);
+                break;
+            case this.next:
+                this.ipo.classList.add("fadeout");
+                this.ipo.classList.add("hidden");
+                let spot =document.querySelector(".spotplay");
+                spot.classList.add("fastfade");
+                spot.classList.remove("hidden")
+                if(this.isPlaying) this.stopAudio();
+                break;
         }
     }
 
-    playAudio(playing){
-        if (!playing){
+    playAudio(){
             this.audio.play();
-            this.playing = true;
-        }
+            this.isPlaying = true;
+    }
+    stopAudio(){
+        this.audio.pause();
+        this.isPlaying = false;
+    }
+    loadMenu(){
+        document.querySelector(".apple").classList.add("hidden");
+        document.querySelector(".iscreen").classList.add("hidden");
+        document.querySelector(".menupage").classList.remove("hidden");
     }
 
 }
