4ab9fcd56058e83282ccc9322ce88031f098999d
kennyvngo <kennyvungo@gmail.com>
2023-05-31 18:40:20 -0700

    phono icon is clickable

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -9,29 +9,35 @@ const spot = document.querySelector(".spotend")
 class Timeline{
     constructor(){
         this.phono = document.querySelector(".phonoend");
+        this.handleClick = this.handleClick.bind(this);
         let time = document.querySelectorAll('.time');
         time.forEach((i) => {
             i.addEventListener('click',this.handleClick)
         })
     }
     
+
+    handleClick(e){
+        
+        if(e.target === document.querySelector(".phonoend")){
+            document.querySelector(".timeline").classList.add("fadeout")
+            setTimeout(this.revealPhono.bind(this),1000)
+            setTimeout(this.hidePhono.bind(this),3000)
+        }
+        }
     revealPhono(){
-        console.log("kenny")
         document.querySelector(".timeline").classList.remove("fadeout")
         document.querySelector(".timeline").classList.add("hidden")
         document.querySelector(".phono").classList.remove("hidden")
         document.querySelector(".phono").classList.add("fastfade")
+        document.querySelector(".bottom").classList.remove("hidden")
+        document.querySelector(".bottom").classList.add("fastfade")
+
     }
-
-    handleClick(e){
-        
-          if(e.target === document.querySelector(".phonoend")){
-            console.log(this.revealPhono);
-            document.querySelector(".timeline").classList.add("fadeout")
-          }
-        }
-
-
+    hidePhono(){
+        document.querySelector(".phono").classList.remove("fastfade")
+        document.querySelector(".bottom").classList.remove("fastfade")
+    }
 }
 
 export default Timeline;
